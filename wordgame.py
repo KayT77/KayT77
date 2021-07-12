fecb9afb81f4ded3234962c261ee25179175338e
kelvin tagoe <tagoe.kelvin07@gmail.com>
2021-07-11 22:53:27 -0600

    Second commit

---
+++ wordgame.py
@@ -0,0 +1,7 @@
+#Word replacement program 
+
+sentence =  input("Enter your sentence: ")
+print ("Your sentence is: "+sentence)
+word1 = input(" Enter  word to replace:")
+word2 = input ("Enter  word to replace it  with:")
+print ( sentence.replace(word1 , word2))
