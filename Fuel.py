d87754ddeb5f5bd5fe58721dea0eda38f2193bd3
kelvin tagoe <tagoe.kelvin07@gmail.com>
2022-10-16 18:01:16 -0600

    back on track

---
+++ Fuel.py
@@ -0,0 +1,27 @@
+import string
+from xmlrpc.client import boolean
+
+
+def init():
+
+   
+    print("Welcome PLS Fuel Services")
+ 
+    EN = int(input("Enter Employee Number:"))
+
+    Odo = int(input("Enter Ododmeter Reading:"))
+
+    pump = int(input("Select Pump:"))
+    
+    print("Fill your Tank, have a NICE DAY")
+    print("SEE YOU SOON, Don't Speed #GOALZERO!!!")
+    
+        
+init()
+
+
+
+
+
+
+
